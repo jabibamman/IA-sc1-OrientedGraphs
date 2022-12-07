74b4dd532aa0b0f870e0b14274ce3fefc1bd0a47
James <james.bidule@gmail.com>
2022-12-07 12:37:13 +0100

    floydWarshall and java func

--- main.py
+++ main.py
@@ -1,50 +1,61 @@
-class Graph:
-    def __init__(self, graph_dict=None):
-        if graph_dict is None:
-            graph_dict = {}
-        self.__graph_dict = graph_dict
+class Graph :
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = [[0 for column in range(vertices)] for row in range(vertices)]
 
-    def vertices(self):
-        return list(self.__graph_dict.keys())
+    def add_vertex(self, vertex) :
+        if vertex < self.V :
+            return
 
-    def add_vertex(self, vertex):
-        if vertex not in self.__graph_dict:
-            self.__graph_dict[vertex] = []
+        self.V += 1
+        for vertex in self.graph :
+            vertex.append(0)
+        self.graph.append([0 for column in range(self.V)])
 
-    def add_edge(self, edge):
-        edge = set(edge)
-        (vertex1, vertex2) = tuple(edge)
-        if vertex1 in self.__graph_dict:
-            self.__graph_dict[vertex1].append(vertex2)
-        else:
-            self.__graph_dict[vertex1] = [vertex2]
 
-    def get_adjacency_list(self):
-        return self.__graph_dict
+    def get_edges(self) :
+        edges = []
+        for vertex in self.graph :
+            for neighbour in self.graph[vertex] :
+                edges.append((vertex, neighbour))
+        return edges
 
-    def get_adjadency_matrix(self):
-        vertices = self.vertices()
-        matrix = [[0 for i in range(len(vertices))] for j in range(len(vertices))]
-        for i in range(len(vertices)):
-            for j in range(len(vertices)):
-                if vertices[j] in self.__graph_dict[vertices[i]]:
-                    matrix[i][j] = 1
-        return matrix
+    def add_edge(self, vertex1, vertex2, weight) :
+        self.graph[vertex1][vertex2] = weight
+        self.graph[vertex2][vertex1] = weight
 
-# call
-if __name__ == "__main__":
+    def get_weight(self, vertex1, vertex2) :
+        return self.graph[vertex1][vertex2]
 
-    g = { "a" : ["d"],
-          "b" : ["c"],
-          "c" : ["b", "c", "d", "e"],
-          "d" : ["a", "c"],
-          "e" : ["c"],
-          "f" : []
-        }
 
-    graph = Graph(g)
-    graph.add_vertex("z")
-    graph.add_edge({"a","z"})
+    def get_adjacency_matrix(self):
+        return self.graph
 
+    def get_adjacency_list(self) :
+        adjacency_list = []
+        for i in range(len(self.graph)) :
+            adjacency_list.append([])
+            for j in range(len(self.graph)) :
+                if j in self.graph[i] :
+                    adjacency_list[i].append(j)
+        return adjacency_list
+
+    def floydWarshall(self, matrix) :
+        dist = matrix
+        for k in range(len(matrix)) :
+            for i in range(len(matrix)) :
+                for j in range(len(matrix)) :
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
+        return dist
+
+if __name__ == "__main__" :
+    graph = Graph(4)
+    graph.add_edge(0, 1, 5)
+    graph.add_edge(0, 3, 10)
+    graph.add_edge(1, 2, 3)
+    graph.add_edge(2, 3, 1)
+    graph.add_vertex(4)
+
+    print(graph.get_adjacency_matrix())
     print(graph.get_adjacency_list())
-    print(graph.get_adjadency_matrix())
+
